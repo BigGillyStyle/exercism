4dfbc64a111b8d50197adaf1e96c9ed143a43c96
Andy Pickler <andy.pickler@gmail.com>
2014-10-30 16:55:49 -0600

    Slight variable rename

--- ruby/gigasecond/gigasecond.rb
+++ ruby/gigasecond/gigasecond.rb
@@ -3,7 +3,7 @@ require 'date'
 class Gigasecond
 
   def self.from(date)
-    gigasecond = 10**9
+    gigaseconds = 10**9
     seconds_per_day = 24*60*60
     date + (gigasecond / seconds_per_day)
   end
