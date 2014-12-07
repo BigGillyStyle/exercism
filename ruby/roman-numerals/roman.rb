e2bcbd3c088b5709e4cf108ac9f8ff98ba615c68
Andy Pickler <andy.pickler@gmail.com>
2014-12-07 07:15:01 -0700

    Much more efficient and clear Roman numerals algorithm

--- ruby/roman-numerals/roman.rb
+++ ruby/roman-numerals/roman.rb
@@ -1,59 +1,29 @@
-class Fixnum
-
-  # f = first
-  # s = second
-  # t = third
-  NUMERALS = {
-    '0' => '',
-    '1' => 'f',
-    '2' => 'ff',
-    '3' => 'fff',
-    '4' => 'fs',
-    '5' => 's',
-    '6' => 'sf',
-    '7' => 'sff',
-    '8' => 'sfff',
-    '9' => 'ft'
-  }
+module RomanNumerals
 
   def to_roman
-    thousands((self / 1000).last_digit) +
-      hundreds((self / 100).last_digit) +
-      tens((self / 10).last_digit) +
-      ones(self.last_digit)
+    remaining = self
+    ROMANS.each_with_object('') do |(arabic, roman), result|
+      result << roman * (remaining / arabic)
+      remaining %= arabic
+    end
   end
 
-  def last_digit
-    self.to_s[-1, 1]
-  end
+  ROMANS = {
+    1000 => 'M',
+    900 => 'CM',
+    500 => 'D',
+    400 => 'CD',
+    100 => 'C',
+    90 => 'XC',
+    50 => 'L',
+    40 => 'XL',
+    10 => 'X',
+    9 => 'IX',
+    5 => 'V',
+    4 => 'IV',
+    1 => 'I'
+  }
 
-  private
-
-  # M = 1000
-  def thousands(t)
-    convert t, 'M', '', ''
-  end
-
-  # D = 500
-  # C = 100
-  def hundreds(h)
-    convert h, 'C', 'D', 'M'
-  end
-
-  # L = 50
-  # X = 10
-  def tens(t)
-    convert t, 'X', 'L', 'C'
-  end
-
-  # V = 5
-  # I = 1
-  def ones(o)
-    convert o, 'I', 'V', 'X'
-  end
-
-  def convert(key, first, second, third)
-    NUMERALS[key].gsub('f',first).gsub('s',second).gsub('t',third)
-  end
+  Fixnum.send :include, RomanNumerals
 
 end
