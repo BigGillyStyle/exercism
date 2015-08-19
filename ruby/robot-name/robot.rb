025a5a927aba1e855f0d86090c2703c05fcae0fc
Andy Pickler <andy.pickler@gmail.com>
2015-08-19 09:33:20 -0600

    First, simplistic solution

---
+++ ruby/robot-name/robot.rb
@@ -0,0 +1,27 @@
+require 'securerandom'
+
+class Robot
+  attr_reader :name
+
+  def initialize
+    generate_random_name
+  end
+
+  def reset
+    generate_random_name
+  end
+
+  private
+
+  def generate_random_name
+    @name = random_letter + random_letter + random_digit + random_digit + random_digit
+  end
+
+  def random_letter
+    ('A'..'Z').to_a.sample
+  end
+
+  def random_digit
+    "#{SecureRandom.random_number(10)}"
+  end
+end
