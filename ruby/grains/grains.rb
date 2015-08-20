42592277f1b457334f4ccf9895b857dc02224552
Andy Pickler <andy.pickler@gmail.com>
2015-08-20 15:58:51 -0600

    Address some Rubocop violations

--- ruby/grains/grains.rb
+++ ruby/grains/grains.rb
@@ -1,11 +1,11 @@
 class Grains
   class << self
     def square(n)
-      2 ** (n-1)
+      2**(n - 1)
     end
 
     def total
-      (1..64).inject {|sum, n| sum += square(n)}
+      (1..64).inject { |a, e| a + square(e) }
     end
   end
 end
