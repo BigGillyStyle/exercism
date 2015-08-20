580a386d739a979166acaed1122f55a651b66057
Andy Pickler <andy.pickler@gmail.com>
2015-08-20 15:15:43 -0600

    More idiomatic Ruby solution

--- ruby/nth-prime/nth_prime.rb
+++ ruby/nth-prime/nth_prime.rb
@@ -2,38 +2,15 @@ class Prime
   class << self
     def nth(nth_prime = 1)
       raise ArgumentError.new('Must provide an integer greater than 0') unless (nth_prime.is_a?(Fixnum) && nth_prime > 0)
-
-      prime_test = 1
-      iter = 1
-      begin
-        if prime_test < 3
-          prime_test += 1
-        else
-          prime_test += 2
-        end
-        iter += 1 if prime_test.is_prime?
-      end while iter <= nth_prime
-      prime_test
-    end
-  end
-end
-
-class Fixnum
-  def is_prime?
-    return false if self < 2
-    return true if self == 2
-
-    prime_divisor = 2
-    upper_bound = Math.sqrt(self).to_i
-    while prime_divisor <= upper_bound do
-      return false if self % prime_divisor == 0
-      if prime_divisor < 3
-        prime_divisor += 1
-      else
-        prime_divisor += 2
-      end
+      @primes = []
+      num = (2..Float::INFINITY).lazy.select {|x| prime?(x)}
+      @primes << num.next until @primes.size == nth_prime
+      @primes.last
     end
 
-    return true
+    def prime?(x)
+      @primes.take_while {|prime| prime ** 2 <= x}
+             .none? {|prime| x % prime == 0}
+    end
   end
 end
