9a6f097f4d5ba6cac32b0e7402e1b70939198746
Andy Pickler <andy.pickler@gmail.com>
2016-09-10 05:43:19 -0600

    Submit leap.js

---
+++ ecmascript/leap/babel-output/leap.js
@@ -0,0 +1,18 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+var isLeapYear = function isLeapYear(year) {
+  if (year % 4 !== 0) {
+    return false;
+  } else if (year % 400 === 0) {
+    return true;
+  } else if (year % 100 === 0) {
+    return false;
+  }
+  return true;
+};
+
+exports["default"] = isLeapYear;
+module.exports = exports["default"];
