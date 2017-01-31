e5c4e10ba5c81c7e6cc8b11f9356982ebe581ce7
Andy Pickler <andy.pickler@gmail.com>
2017-01-31 09:52:11 -0700

    First attempt with some failures

--- ecmascript/bob/bob.js
+++ ecmascript/bob/bob.js
@@ -1,15 +1,14 @@
-//
-// This is only a SKELETON file for the 'Bob' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 class Bob {
   hey(message) {
-    //
-    // YOUR CODE GOES HERE
-    //
+    if (!message || message.trim().length == 0) {
+      return 'Fine. Be that way!';
+    } else if (message.endsWith('?')) {
+      return 'Sure.';
+    } else if (message.endsWith('!')) {
+      return 'Whoa, chill out!';
+    }
+    return 'Whatever.';
   }
 }
 
 export default Bob;
-
