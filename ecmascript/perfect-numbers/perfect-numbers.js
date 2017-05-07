682a105d91610800e4c462a3d3cb9ee4f406f3cd
Andy Pickler <andy.pickler@gmail.com>
2017-05-07 07:53:52 -0600

    Working for all tests, but slow

---
+++ ecmascript/perfect-numbers/perfect-numbers.js
@@ -0,0 +1,25 @@
+class PerfectNumbers {
+  classify(num) {
+    if (num < 1) throw 'Classification is only possible for natural numbers.';
+
+    const aliquotSum = this.factors(num).reduce((acc, val) => acc + val, 0);
+    if (aliquotSum === num) {
+      return 'perfect';
+    } else if (aliquotSum > num) {
+      return 'abundant';
+    }
+    return 'deficient';
+  }
+
+  factors(num) {
+    const startNum = Math.floor(num / 2);
+    const intRange = [...Array(startNum).keys()].map(i => i+1);
+    return intRange.filter(div => this.isDivisor(num, div));
+  }
+
+  isDivisor(num, div) {
+    return num % div === 0;
+  }
+}
+
+export default PerfectNumbers;
