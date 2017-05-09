343a7d94bf4da29cc6de51a8f5bc0a6604a2799d
Andy Pickler <andy.pickler@gmail.com>
2017-05-09 16:09:33 -0600

    First passing of all tests

---
+++ ecmascript/beer-song/beer-song.js
@@ -0,0 +1,34 @@
+class Beer {
+  static verse(bottleNum) {
+    return `${Beer.numToBottleString(bottleNum, true)} of beer on the wall, ${Beer.numToBottleString(bottleNum)} of beer.
+${Beer.secondSentence(bottleNum)}
+`;
+  }
+
+  static numToBottleString(bottleNum, startOfPhrase = false) {
+    const word = startOfPhrase ? 'No' : 'no'
+    const num = bottleNum > 0 ? bottleNum : `${word} more`;
+    return `${num} bottle${num !== 1 ? 's' : ''}`;
+  }
+
+  static secondSentence(bottleNum) {
+    const nextBottleNum = bottleNum - 1;
+    if (nextBottleNum >= 0) {
+      return `Take ${nextBottleNum > 0 ? 'one' : 'it'} down and pass it around, ${Beer.numToBottleString(nextBottleNum)} of beer on the wall.`;
+    } else {
+      return 'Go to the store and buy some more, 99 bottles of beer on the wall.'
+    }
+  }
+
+  static sing(from = 99, to = 0) {
+    let song = '';
+    while (from >= to) {
+      song += Beer.verse(from);
+      from -= 1;
+      if (from >= to) song += '\n';
+    }
+    return song;
+  }
+}
+
+export default Beer;
