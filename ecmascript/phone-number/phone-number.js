2735a95981cb106a83fd1593e6d40ec879d7fda6
Andy Pickler <andy.pickler@gmail.com>
2017-05-10 16:35:30 -0600

    More elegant, compact solution

--- ecmascript/phone-number/phone-number.js
+++ ecmascript/phone-number/phone-number.js
@@ -1,15 +1,12 @@
 class PhoneNumber {
   constructor(phoneNum) {
-    this.phoneNum = phoneNum;
+	  this.phoneNum = phoneNum;
   }
 
   number() {
-    if (this.phoneNum.match(/[a-z]/gi)) return null;
-    let digitsOnly = this.phoneNum.match(/[0-9]/g);
-    const len = digitsOnly.length;
-    if (len < 10 || len > 11 || len === 11 && digitsOnly[0] !== '1') return null;
-    if (digitsOnly.length === 11) digitsOnly.splice(0, 1);
-    return digitsOnly.join('');
+    const sanitzedPhoneNum = this.phoneNum.replace(/[\(\)\. \-]/g, "");
+	  const match = sanitzedPhoneNum.match(/^1?(\d{10})$/);
+	  return match && match[1];
   }
 }
 
