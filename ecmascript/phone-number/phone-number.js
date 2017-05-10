1957ec8ddbdaa4a199b81998fd6d3d5b30ceaa94
Andy Pickler <andy.pickler@gmail.com>
2017-05-10 16:39:47 -0600

    Tabs to spaces

--- ecmascript/phone-number/phone-number.js
+++ ecmascript/phone-number/phone-number.js
@@ -1,12 +1,12 @@
 class PhoneNumber {
   constructor(phoneNum) {
-	  this.phoneNum = phoneNum;
+    this.phoneNum = phoneNum;
   }
 
   number() {
     const sanitzedPhoneNum = this.phoneNum.replace(/[\(\)\. \-]/g, "");
-	  const match = sanitzedPhoneNum.match(/^1?(\d{10})$/);
-	  return match && match[1];
+    const match = sanitzedPhoneNum.match(/^1?(\d{10})$/);
+    return match && match[1];
   }
 }
 
